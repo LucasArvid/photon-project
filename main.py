e05fcd96e24a06132699c141f8067335f27d1d96
Lucas Arvidsson <Lucas.arvidsson@hotmail.com>
2020-06-20 22:49:33 +0200

    implemented imp/kWh tracker

--- main.py
+++ main.py
@@ -1,98 +1,95 @@
-import socket
-import time
 import pycom
 import machine
-import binascii
-import struct
-import ubinascii
-from machine import Pin
-from network import LoRa
+from machine import Timer
+import time
+import _thread
 
-GREEN = 0x007f00
-RED = 0x7f0000
-YELLOW = 0x7f7f00
-OFF = 0x000000
+counter = 0
 
+# 10 000 imp/kWh -> 166,667 imp/kWh per min -> 2.778 imp/kWh per sec
 
-lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
+# Timer for sending data to pybytes, should be periodicly sent depending on the interval of the power indicator
+class Data_Interupt:
 
-THE_APP_EUI = '70B3D57ED00307B8'
-THE_APP_KEY = '2A931242F9C54966445410B1E867D81D'
-app_eui = ubinascii.unhexlify('70B3D57ED00307B8')
-app_key = ubinascii.unhexlify('2A931242F9C54966445410B1E867D81D')
+    def __init__(self):
+        self.seconds = 0
+        self.led = 1
+        self.__alarm = Timer.Alarm(self._seconds_handler, 60, periodic=True)
+        self.__alarm2 = Timer.Alarm(self._led_handler, 0.5, periodic=True)
 
-lora = LoRa(mode=LoRa.LORAWAN)
-
-
-adc = machine.ADC()
-apin = adc.channel(pin="P16")
-val = 0
-dac = machine.DAC("P22")
-
-def set_led_to(color=GREEN):
-    pycom.heartbeat(False)
-    pycom.rgbled(color)
-
-def flash_led_to(color=GREEN, t1=1):
-    set_led_to(color)
-    time.sleep(t1)
-    set_led_to(OFF)
-
-def join_lora(force_join = False):
-    print("Connecting to TTN")
-
-    if not force_join:
-        print("restoring state")
-        lora.nvram_restore()
-
-    if not lora.has_joined() or force_join == True:
-
-        # Join netowkr using OTAA
-        lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
-
-        # Wait for module to join network
-        while not lora.has_joined():
-            print("Not joined yet...")
-            time.sleep(2.5)
-
-        lora.nvram_save()
-
-        # returning whether the join was successful
-        if lora.has_joined():
-            flash_led_to(GREEN)
-            print('LoRa Joined')
-            return True
-        else:
-            flash_led_to(RED)
-            print('LoRa Not Joined')
-            return False
-
-    else:
-        return True
-
-print("Device LoRa MAC:", binascii.hexlify(lora.mac()))
-
-pycom.heartbeat(False)
-
-flash_led_to(YELLOW)
-# Join LoRa
-join_lora(True)
-
-while True:
-    # LoRa Socket
-    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-    s.setsockopt(socket.SOL_LORA, socket.SO_DR, 0)
-    s.setblocking(True)
-
-    val = apin()
-    dac.write((val/100))
-
-    payload = struct.pack(">fff", val)
-
-    # s.send(payload)
-    s.setblocking(False)
-    flash_led_to(GREEN)
-    print("iterated")
-    time.sleep(2)
-
+    def _seconds_handler(self, alarm):
+        global counter
+        kWh = self._calculate_power()
+        self.seconds += 10
+        print("kWh: " + str(kWh))
+        print("%02d seconds have passed" % self.seconds)
+        pybytes.send_signal(0, kWh)
+        counter = 0
     
+    def _led_handler(self, alarm):
+        dac = machine.DAC('P22')        # create a DAC object
+        self.led *= -1 
+        dac.write(self.led)
+       # print("led")
+
+    def _calculate_power(self):
+        global counter
+        total_imp = counter * 60
+        total_imp = total_imp / 10000
+        return total_imp
+
+send_data = Data_Interupt()
+
+
+# function for updating a counter each time the power indicator lights up, should be reset each interval
+def track_light():
+    adc = machine.ADC()             # create an ADC object
+    apin = adc.channel(pin='P16') 
+    global counter
+    light = False
+
+
+    while True:
+        val = apin()    
+        val= val/400
+        val= val/10
+
+        if val >= 0.5 and light == False:
+            print("Light")
+            counter += 1
+            print(str(val))
+            print(str(light))
+            print("")
+            light = True
+
+        elif val < 0.5 and light == True:
+            print("Dark")
+            print(str(val))
+            print(str(light))
+            print("")
+            light = False
+
+
+# currently only used to light the LED depending on the photon sensors values.
+def run_loop():
+    dac = machine.DAC('P22')        # create a DAC object
+    adc = machine.ADC()             # create an ADC object
+    apin = adc.channel(pin='P16') 
+    global counter
+
+    while True:
+        time.sleep(1)
+        val = apin()
+        val= val/400
+        val= val/10
+        dac.write(val)
+        print(str(counter))
+
+
+    # pybytes.send_signal(1, val)
+    # time.sleep(10)
+
+
+
+_thread.start_new_thread(track_light, ())
+#_thread.start_new_thread(run_loop, ())
